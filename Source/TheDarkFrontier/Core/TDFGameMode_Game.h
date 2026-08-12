516deab525e9020e49448cd372602e32150900a3
Matt B <cronos1979@live.com>
2026-08-11 20:11:02 -0500

    Mainmenu and loading screen done

---
+++ Source/TheDarkFrontier/Core/TDFGameMode_Game.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TDFGameMode_Game.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEDARKFRONTIER_API ATDFGameMode_Game : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
